2596d6d4de25194686a62b85676bfd4922defd50
taehyun <tanggu01@connect.hku.hk>
2024-04-04 20:03:24 -0400

    :sparkles: feat: A5 - 4.Implementing the Kanbas Node.js HTTP Server

--- App.js
+++ App.js
@@ -6,13 +6,15 @@
 // equivalent to import; before es6
 // const express = require("express");
 import cors from "cors";
+import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
 
 
 // common.js is default: package.json
 import express from "express";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
-
+import Modules from "./Kanbas/Database/modules.js";
 //create instance
 const app = express();
 app.use(cors());
@@ -20,7 +22,8 @@ app.use(express.json());
 
 Lab5(app); // pass reference to express module
 Hello(app); //pass app instance
+CourseRoutes(app);
+ModuleRoutes(app);
 
-
-app.listen(4000); //react is 3000
+app.listen(process.env.PORT || 4000);
 //run server by ' node App.js'
